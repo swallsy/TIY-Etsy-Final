b7c90f27756df89efa384f27f6c72e7f06385b0f
Michael Ashton <mykl.ashton@gmail.com>
2017-07-24 13:26:32 -0400

    Initial file structure

---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../../styles/Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+          {/* <Header /> */}
+          {/* <Carousel /> */}
+          {/* <InfoCards /> */}
+          {/* <ShopCards {category} /> */}
+          {/* <BrowseCollections /> */}
+          {/* <ShopCards {gifts} /> */}
+          {/* <OrangeBar {safety} /> */}
+          {/* <RecentReviews /> */}
+          {/* <OrangeBar {selling} /> */}
+          {/* <FreshBlog /> */}
+          {/* <OrangeBar {WhatIsEtsy} /> */}
+          <h1>You're good to go. Now make Etsy!</h1>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Home from './components/Home/Home.js';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
