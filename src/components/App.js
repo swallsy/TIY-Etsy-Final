1ace9c12262d37a949e6e6cbd09525d44c22d72d
Jason Grigg <boatdreamin@gmail.com>
2017-07-24 16:19:36 -0400

    App.js creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+import AllCategories from './CatBar/PlayList.js';
+import Color from './CatBar/Color.js';
+import ItemType from './CatBar/ItemType.js';
+import OrderingOptions from './CatBar/OrderingOptions.js';
+import Price from './CatBar/Price.js';
+import ShipTo from './CatBar/ShipTo.js';
+import ShopLocation from './CatBar/ShopLocation.js';
+import CatCards from './CatCards/CatCards.js';
+import DetailedCatCards from './CatCards/DetailedCatCards.js';
+import ProductCards from './CatCards/ProductCards.js';
+import CatBar from '../CatBar.js';
+import PageCounter from '../PageCounter.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AllCategories />
+        <Color />
+        <ItemType />
+        <OrderingOptions />
+        <Price />
+        <ShipTo />
+        <ShopLocation />
+        <CatCards />
+        <DetailedCatCards />
+        <ProductCards />
+        <CatBar />
+        <PageCounter />
       </div>
     );
   }
