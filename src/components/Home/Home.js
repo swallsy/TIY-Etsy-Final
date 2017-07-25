51d51b20b98118f7a832feca167d6bb5f1f91b46
Matt McDonald <mattmcd1221@gmail.com>
2017-07-25 15:15:34 -0400

    merge conflict

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import '../../styles/Home.css';
-<<<<<<< HEAD
-import Nav from '../Header/Nav.js'
 import OrangeBar from './OrangeBar.js'
-=======
-import OrangeBar from './OrangeBar/OrangeBar.js'
->>>>>>> f96521f5abc7cf37d8250008b160fb2180fe96c2
 import ShopCards from './ShopCards/ShopCards.js'
 import Carousel from './Carousel.js'
 import InfoCards from './InfoCards.js';
@@ -16,11 +11,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className='Home'>
-<<<<<<< HEAD
-          {/* <Header /> */}
-=======
           <Header />
->>>>>>> f96521f5abc7cf37d8250008b160fb2180fe96c2
           <Carousel />
           <InfoCards />
           <ShopCards type="category" />
