f6c7ccc682368b303bbd99065081bff0855a165d
Garmhausen <robertgarmhausen@gmail.com>
2017-07-24 20:33:16 -0400

    Remove '.js' from end of imported components.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
-import ShopHeader from './ShopHeader.js'
-import LikeBanner from './LikeBanner.js'
-import ProductImage from './ProductImage.js'
-import PurchaseInfo from './PurchaseInfo.js'
-import ProdDesc from './ProdDesc.js'
-import ShopSidebar from './ShopSidebar.js'
+import ShopHeader from './ShopHeader'
+import LikeBanner from './LikeBanner'
+import ProductImage from './ProductImage'
+import PurchaseInfo from './PurchaseInfo'
+import ProdDesc from './ProdDesc'
+import ShopSidebar from './ShopSidebar'
 
 class App extends Component {
   render() {
