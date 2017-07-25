242aa7d2cdac5e5cef2af7cf7328142c252d981a
Jason Grigg <boatdreamin@gmail.com>
2017-07-25 11:19:22 -0400

    changes to file routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import AllCategories from './CatBar/PlayList.js';
+import AllCategories from './CatBar/AllCategories.js';
 import Color from './CatBar/Color.js';
 import ItemType from './CatBar/ItemType.js';
 import OrderingOptions from './CatBar/OrderingOptions.js';
@@ -10,8 +10,8 @@ import ShopLocation from './CatBar/ShopLocation.js';
 import CatCards from './CatCards/CatCards.js';
 import DetailedCatCards from './CatCards/DetailedCatCards.js';
 import ProductCards from './CatCards/ProductCards.js';
-import CatBar from '../CatBar.js';
-import PageCounter from '../PageCounter.js';
+import CatBar from '../components/CatBar.js';
+import PageCounter from '../components/PageCounter.js';
 
 
 class App extends Component {
