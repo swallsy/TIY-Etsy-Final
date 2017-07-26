958fc6ecc0c10421137e44a3087a220b58f266f3
Garmhausen <robertgarmhausen@gmail.com>
2017-07-26 16:02:54 -0400

    Sidebar columns are getting there.

--- src/components/ShopSidebar.js
+++ src/components/ShopSidebar.js
@@ -33,7 +33,7 @@ class ShopSidebar extends Component {
         // now let's iterate through the tempListings array and make some renderable array entries for the main render below
         let shopListingsCards = tempListings.map(listing => {
           return (
-            <div className="col-6 shop-listings-card" key={listing.listing_id}>
+            <div className="col-6 card-sm shop-listings-card" key={listing.listing_id}>
               <a href={listing.url}>
                 <div>
                   <img
@@ -50,12 +50,22 @@ class ShopSidebar extends Component {
           );
         });
 
+        let shopListingsGroups = [];
+        for (let i = 0; shopListingsGroups.length < 4; i += 2) {
+          shopListingsGroups.push(
+              <div className="row">
+                {shopListingsCards[i]}
+                {shopListingsCards[i+1]}
+              </div>
+          )
+        }
+
         this.setState({
           results: response.results,
           shop_name: response.results[0].Shop.shop_name,
           shop_url: response.results[0].Shop.url,
           shop_picurl: response.results[0].Shop.icon_url_fullxfull,
-          shop_listings: shopListingsCards
+          shop_listings: shopListingsGroups
         });
       });
   }
