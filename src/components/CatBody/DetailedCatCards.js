05446703fb26b45a0770545bc0012edb4622c236
Lorien Olive <lorien.olive@gmail.com>
2017-07-26 09:44:00 -0400

    adds randomizer to fetch for DetailedCatCard

--- src/components/CatBody/DetailedCatCards.js
+++ src/components/CatBody/DetailedCatCards.js
@@ -1,69 +1,73 @@
 import React, { Component } from 'react';
 
 
+
+
 class DetailedCatCard extends Component {
   constructor(props) {
-    super(props)
-      // render() {
+    super(props);
 
-        let listings = this.props.listings.map(listing => {
-
-        if ( listing.category_id === 68887482 ) {
-          let key = Math.random().toString(36).substring(7);
-          console.log(key);
-
-        }
-      })
+    this.state = {
+      listings: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://api.etsy.com/v2/listings/active?api_key=3yhxu7gn2ot24so9hzuqbxc9&limit=100&offset=0&page=1&categories=jewelry,craft_supplies_and_tools,home_and_living,art_and_collectibles,clothing,accessories,paper_and_party_supplies,weddings,bags_and_purses&includes=MainImage')
+    .then(resp => resp.json())
+      .then(resp => {
+        console.log(resp)
+        let listings = resp.results
+        // eslint-disable-next-line
+        let listingCard = listings.map((listing) => {
+          if (listing.category_path.includes("Jewelry")) {
+            console.log(listings)
+            let jewelryListings = listings.filter((jewelryListing) => {
+              return jewelryListing.category_path.includes("Jewelry");
+            })
+            console.log(jewelryListings);
+            let jewelryListing = jewelryListings[Math.floor(Math.random() * jewelryListings.length)]
+              console.log(jewelryListing);
+              return (
+                <div key={jewelryListing.id} className = "col-md-4">
+                  <div className="card" >
+                    <img className="card-img-top" src="{jewelryListing.MainImage.url_170x135}" alt="random jewelry" />
+                    <div className="card-block">
+                      <h2 className="card-title">
+                        <a to="https://www.etsy.com/c/jewelry">Jewelry</a>
+                      </h2>
+                      <p className="card-text">
+                        <a to="https://www.etsy.com/c/jewelry/necklaces">Necklaces</a>
+                      </p>
+                      <p className="card-text">
+                        <a to="https://www.etsy.com/c/jewelry/earrings">Earrings</a>
+                      </p>
+                      <p className="card-text">
+                        <a to="https://www.etsy.com/c/jewelry/bracelets">Bracelets</a>
+                      </p>
+                      <p className="card-text">
+                        <a to="https://www.etsy.com/c/jewelry/necklaces">Rings</a>
+                      </p>
+                        <a to="https://www.etsy.com/c/jewelry">Shop All Jewelry</a>
+                    </div>
+                  </div>
+                </div>
+              )
+            }
+          })
+          this.setState({listings: listingCard})
+          console.log(this.state.listings);
+        })
+      }
+      render() {
+      return (
+        <div className="container">
+          {this.state.listing}
+        </div>
+      )
     }
   }
 
 
+
+
 export default DetailedCatCard
-
-
-
-
-
-
-
-
-
-
-
-
-    //     return (
-    //       <div key = {listing.id} className = "col-md-4">
-    //         <div className="card" >
-    // //I'm a little unsure here of how we will select an image, since we're querying
-    // //for category not for particular products. Thoughts?
-    //             <img className="card-img-top" src="{listing.image}" />
-    //             <div className="card-block">
-    //               <h2 className="card-title">
-    //                 <button><Link to="https://www.etsy.com/c/jewelry">{Jewelry}</Link></button>
-    //               </h2>
-    // //Also here, we will need to figure out how to select multiple sub-categories
-    // //and list them here. Do we need a second .map thing so we can loop through
-    // //the array of sub-categories?
-    //               <p className="card-text">
-    //                 <button><Link to="https://www.etsy.com/c/jewelry/necklaces">Necklaces</Link></button>
-    //               </p>
-    //               <p className="card-text">
-    //                 <button><Link to="https://www.etsy.com/c/jewelry/earrings">Earrings</Link></button>
-    //               </p>
-    //               <p className="card-text">
-    //                 <button><Link to="https://www.etsy.com/c/jewelry/bracelets">Bracelets</Link></button>
-    //               </p>
-    //               <p className="card-text">
-    //                 <button><Link to="https://www.etsy.com/c/jewelry/necklaces">Rings</Link></button>
-    //               </p>
-    //               <button><Link to="https://www.etsy.com/c/jewelry">Shop All Jewelry</Link></button>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       )
-    //     })
-    //     return (
-    //       <div className="container">
-    //         {detailedCatItem}
-    //       </div>
-    //     )
