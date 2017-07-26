9fb1daf94f1b2a06cc9b947fd155f2cb1d4f2922
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-07-26 19:38:36 -0400

    fix text alignment.

--- src/components/Home/FreshBlog.js
+++ src/components/Home/FreshBlog.js
@@ -12,11 +12,11 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/featured-shop-emma-claire/?ref=hp" alt="featured shop by emma claire. Blog Post.">
                 <img className="card-img-top blogimg" src={require("./images/etsypic1.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">FEATURED SHOPS</h5>
+                <div className="blogtext"><h5 className="card-text blogcategories">FEATURED SHOPS</h5>
                 <h4 className="card-title">Featured Shop: Emma Claire</h4>
                 <p className="card-text">
                   Bring the outdoors in with botanical home accessories from these green-thumbed friends and co-founders.
-                </p>
+                </p></div>
             </a>
           </div>
           </div>
@@ -28,11 +28,11 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/are-you-pro-pineapple-or-team-watermelon/?ref=hp" alt="Shopping guides, are you pro pineapple or watermelon">
                 <img className="card-img-top blogimg" src={require("./images/etsypic2.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">SHOPPING GUIDES</h5>
+                <div className="blogtext"><h5 className="card-text blogcategories">SHOPPING GUIDES</h5>
                 <h4 className="card-title">Are You Pro-Pineapple or Team Watermelon?</h4>
                 <p className="card-text">
                   Discover what your favorite trending fruit motif says about you.
-                </p>
+                </p></div>
             </a>
           </div>
           </div>
@@ -44,11 +44,11 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/the-ultimate-diy-birthday-party-playbook/?ref=hp" alt="Inspiration, the ultimate diy birthday party playbook.">
                 <img className="card-img-top blogimg" src={require("./images/etsypic3.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">INSPIRATION</h5>
+                <div className="blogtext"><h5 className="card-text blogcategories">INSPIRATION</h5>
                 <h4 className="card-title">The Ultimate DIY Birthday Party Playbook</h4>
                 <p className="card-text">
                   Mix and match your favorite handmade ideas—from a sweet (and easy) dessert station to favors guests will treasure—for a more personalized party.
-                </p>
+                </p></div>
             </a>
           </div>
           </div>
