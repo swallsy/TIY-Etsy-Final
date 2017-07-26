e4c919c7c202f9b2b5247861a2a03913d607b727
Jason Grigg <boatdreamin@gmail.com>
2017-07-26 16:32:15 -0400

    Product Cards Modifiations

--- src/components/CatBody/ProductCards.js
+++ src/components/CatBody/ProductCards.js
@@ -9,15 +9,16 @@ export default class ProductCards extends Component {
   }
 
   componentDidMount() {
-    fetch('https://openapi.etsy.com/v2/listings/active?api_key=3yhxu7gn2ot24so9hzuqbxc9&fields=title,url&limit=10&includes=Images,Shop').then(resp => resp.json())
+    fetch('https://api.etsy.com/v2/listings/trending?api_key=3yhxu7gn2ot24so9hzuqbxc9&limit=10&offset=0&page=1&categories=jewelry,craft_supplies_and_tools,home_and_living,art_and_collectibles,clothing,accessories,paper_and_party_supplies,weddings,bags_and_purses&includes=MainImage,Shop').then(resp => resp.json())
     .then(resp => {
       let productCards = resp.results.map((product) => {
         return (
-          <div className="card col-md-3" key={product.Images.listing_image_id}>
-            <img src={product.Images[0].url_75x75} />
+          <div className="card col-md-3" key={product.MainImage.listing_image_id}>
+            <img src={product.MainImage.url_170x135} />
             <div>{product.title}</div>
             <div>{product.Shop.shop_name}</div>
-            <div>{product.Images.rank}</div>
+            <div>({product.Shop.num_favorers})</div>
+            <div>${product.price}</div>
           </div>
         )
       })
