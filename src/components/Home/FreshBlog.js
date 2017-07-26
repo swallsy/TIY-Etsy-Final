60c7da8dd83c9da90909cc8f89d9552cf444ad93
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-07-26 19:23:07 -0400

    bold the titles of the blog cards.

--- src/components/Home/FreshBlog.js
+++ src/components/Home/FreshBlog.js
@@ -12,7 +12,7 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/featured-shop-emma-claire/?ref=hp" alt="featured shop by emma claire. Blog Post.">
                 <img className="card-img-top blogimg" src={require("./images/etsypic1.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">Featured Shops</h5>
+                <h5 className="card-text">FEATURED SHOPS</h5>
                 <h4 className="card-title">Featured Shop: Emma Claire</h4>
                 <p className="card-text">
                   Bring the outdoors in with botanical home accessories from these green-thumbed friends and co-founders.
@@ -28,7 +28,7 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/are-you-pro-pineapple-or-team-watermelon/?ref=hp" alt="Shopping guides, are you pro pineapple or watermelon">
                 <img className="card-img-top blogimg" src={require("./images/etsypic2.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">Shopping Guides</h5>
+                <h5 className="card-text">SHOPPING GUIDES</h5>
                 <h4 className="card-title">Are You Pro-Pineapple or Team Watermelon?</h4>
                 <p className="card-text">
                   Discover what your favorite trending fruit motif says about you.
@@ -44,8 +44,8 @@ class FreshBlog extends Component {
             <div className="card-block">
               <a href="https://blog.etsy.com/en/the-ultimate-diy-birthday-party-playbook/?ref=hp" alt="Inspiration, the ultimate diy birthday party playbook.">
                 <img className="card-img-top blogimg" src={require("./images/etsypic3.jpg")} alt="Card image cap"/>
-                <h5 className="card-text">Inspiration</h5>
-                <h4 className="card-title">The Ultimate DIYBirthday Party Playbook</h4>
+                <h5 className="card-text">INSPIRATION</h5>
+                <h4 className="card-title">The Ultimate DIY Birthday Party Playbook</h4>
                 <p className="card-text">
                   Mix and match your favorite handmade ideas—from a sweet (and easy) dessert station to favors guests will treasure—for a more personalized party.
                 </p>
