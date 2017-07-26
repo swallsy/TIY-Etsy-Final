869e84129ba1fade32e753af8a6ccf6a1d4112cc
Garmhausen <robertgarmhausen@gmail.com>
2017-07-26 14:03:15 -0400

    First Draft of ShopHeader component.

--- src/components/ShopHeader.js
+++ src/components/ShopHeader.js
@@ -1,51 +1,71 @@
-import React, { Component } from "react";
-import "../styles/App.css";
+import React, { Component } from 'react';
+import '../styles/App.css';
 
 class ShopHeader extends Component {
   constructor() {
     super();
+
     this.state = {
-      shop: {}
+      results: {},
+      shop_name: '',
+      shop_url: '',
+      shop_picurl: '',
+      shop_listings: [],
+      shop_listings_count: ''
     };
   }
+
   componentDidMount() {
     fetch(
-      "https://openapi.etsy.com/v2/shops/listing/175112598?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
+      'https://openapi.etsy.com/v2/listings/175112598?includes=Shop/Listings/MainImage&api_key=nrfza0h31bu4g5biq6bq6g4c'
     )
       .then(response => response.json())
       .then(response => {
-        console.log(response);
-				let shopCards = response.results[0].Listings.map(shopCard => {
-					return(
-						<div>
-
-						</div>
-					)
-				})
-        this.setState({
-          shop: {
-            shop_id: response.results[0].shop_id,
-            shop_name: response.results[0].shop_name,
-            shop_url: response.results[0].url
+        let tempListings = [];
+        for (let i = 0; tempListings.length < 4; i++) {
+          if (response.results[0].Shop.Listings[i].MainImage) {
+            tempListings.push(response.results[0].Shop.Listings[i]);
           }
+        }
+        let shopListingsThumbs = tempListings.map(listing => {
+          return (
+            <div className="shopheader-listings-thumb" key={listing.listing_id}>
+              <a href={listing.url}>
+                <img
+                  src={listing.MainImage.url_fullxfull}
+                  className="shopheader-listings-thumb-pic"
+                />
+              </a>
+            </div>
+          );
         });
-        // fetch(
-        //   "https://openapi.etsy.com/v2/shops/"+this.state.shop.shop_id+"?includes=Listings&api_key=nrfza0h31bu4g5biq6bq6g4c"
-        // )
-        //   .then(response => response.json())
-        //   .then(
-				// 		console.log(response)
-				// 	);
-      })
-      .catch(error => {
-        console.error(error);
+        this.setState({
+          results: response.results,
+          shop_name: response.results[0].Shop.shop_name,
+          shop_url: response.results[0].Shop.url,
+          shop_picurl: response.results[0].Shop.icon_url_fullxfull,
+          shop_listings: shopListingsThumbs,
+          shop_listings_count: response.results[0].Shop.listing_active_count
+        });
       });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="ShopHeader col-12 bg-faded">
-        <p>ShopHeader code needed.</p>
+        <a href={this.state.shop_url}>
+          <img src={this.state.shop_picurl} className="shopheader-shop-pic" />
+        </a>
+        <a href={this.state.shop_url}>
+          {this.state.shop_name}
+        </a>
+        {this.state.shop_listings}
+        <a href={this.state.shop_url}>
+          <div className="shopheader-listings-thumb">
+            {this.state.shop_listings_count} items
+          </div>
+        </a>
       </div>
     );
   }
