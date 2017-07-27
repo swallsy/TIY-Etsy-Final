00db5902a84779a18ae57c06e56049c0de11cab8
Lorien Olive <lorien.olive@gmail.com>
2017-07-27 14:39:59 -0400

    move fetch to CatBody and passed state as props to child components

--- src/components/CatBody.js
+++ src/components/CatBody.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
-import CatCards from './CatBody/CatCards.js';
+import CatCard from './CatBody/CatCards.js';
 import DetailedCatCard from './CatBody/DetailedCatCards.js';
-import ProductCards from './CatBody/ProductCards.js';
+
 
 class CatBody extends Component {
   constructor(props) {
@@ -11,145 +11,160 @@ class CatBody extends Component {
     this.state = {
       listings: []
     }
+  };
 
-}
   componentDidMount() {
-    fetch('https://api.etsy.com/v2/listings/active?api_key=3yhxu7gn2ot24so9hzuqbxc9&categories=jewelry,craft_supplies_and_tools,home_and_living,art_and_collectibles,clothing,accessories,paper_and_party_supplies,weddings,bags_and_purses&includes=MainImage')
-    .then(results => {
-      return results.json();
+    fetch('https://api.etsy.com/v2/listings/trending?api_key=3yhxu7gn2ot24so9hzuqbxc9&explicit=1&amp;min=&amp;max=&amp;price_bucket=1&amp;use_mmx=1&&limit=100&offset=0&page=1&category=clothing&category=Accessories&category=paper_party_supplies&category=weddings&bags_and_purses&category=art_and_collectibles&category=jewelry&category=home_and_living&category=craft_supplies_and_tools&includes=MainImage')
+    .then(resp => {
+      return resp.json()
     })
-      .then(data => {
-        this.setState({listings: data});
+      .then(resp => {
+        console.log(resp);
+        let listings = resp.results;
+        this.setState({listings: listings});
         console.log("state", this.state.listings);
       })
-  }
+    }
+
 
   render() {
-    return (
-      <div className = "container" >
-        <DetailedCatCard listings={this.state.listings} />
-      </div>
-      )
-   }
+    if (this.state.listings) {
+    const detailedCategories = [
+        {
+          category: {
+            name: "Jewelry",
+            path: "jewelry/"
+          },
+          subCategories: {
+            cat1: {
+              name: "Necklaces",
+              path: "necklaces"
+            },
+            cat2: {
+              name: "Earings",
+              path: "earings"
+            },
+            cat3: {
+              name: "Bracelets",
+              path: "bracelets"
+            },
+            cat4: {
+              name: "Rings",
+              path: "rings"
+            }
+          }
+        },
+        {
+          category: {
+            name: "Craft Supplies & Tools",
+            path: "craft_supplies_and_tools/"
+          },
+          subCategories: {
+            cat1: {
+              name: "Jewelry & Beauty",
+              path: "jewelry_and_beauty"
+            },
+            cat2: {
+              name: "Sewing & Fiber",
+              path: "sewing_and_fiber"
+            },
+            cat3: {
+              name: "Paper, Party & Kids",
+              path: "paper_party_and_kids"
+            },
+            cat4: {
+              name: "Visual Arts",
+              path: "visual_arts"
+            }
+          }
+        },
+        {
+          category: {
+            name: "Home & Living",
+            path: "home_and_living/"
+          },
+          subCategories: {
+            cat1: {
+              name: "Home Décor",
+              path: "home_decor"
+            },
+            cat2: {
+              name: "Kitchen & Dining",
+              path: "kithen_and_dining"
+            },
+            cat3: {
+              name: "Bedding",
+              path: "bedding"
+            },
+            cat4: {
+              name: "Outdoor & Gardening",
+              path: "outdoor_and_gardening"
+            }
+          }
+        },
+        {
+          category: {
+            name: "Art & Collectibles",
+            path: "art_and_collectibles/"
+          },
+          subCategories: {
+            cat1: {
+              name: "Prints",
+              path: "prints"
+            },
+            cat2: {
+              name: "Collectibles",
+              path: "collectibles"
+            },
+            cat3: {
+              name: "Painting",
+              path: "painting"
+            },
+            cat4: {
+              name: "Photography",
+              path: "photography"
+            }
+          }
+        }
+      ];
+      const categories = [
+          {category: {
+            name: "Clothing",
+            path: "clothing"
+          }},
+          {category: {
+            name: "Accessories",
+            path: "accessories"
+          }},
+          {category: {
+            name: "Paper & Party Supplies",
+            path: "paper_and_party_suppies"
+          }},
+          {category: {
+            name: "Weddings",
+            path: "weddings"
+          }},
+          {category: {
+            name: "Bags & Purses",
+            path: "bags_and_purses"
+          }}
+        ];
+          return (
+            <div className = "container" >
+              {this.state.listings.length > 0 ? (
+              <div>
+                <DetailedCatCard detailedCategories={detailedCategories}
+                         etsyListings={this.state.listings} />
+                <CatCard categories={categories}
+                         etsyListings={this.state.listings} />
+              </div>
+              ) : (
+              <div>Loading</div>
+              )}
+            </div>
+          )
+    }
+  }
 }
 
 export default CatBody
-
-
-
-
-
-
-
-
-
-
-
-
-
-//       return (
-//         <div key={key} className="col-sm-6 col-md-4">
-//           <div className="thumbnail">
-//             <img className="" src={imgUrl} alt={description}/>
-//             <div className="caption">
-//               <p className="lead truncate">{title}</p>
-//               <hr className="divider"/>
-//               <p className="text-center truncate">{description}</p>
-//               <span className="text-center pull-left">{author || 'IGN'}</span>
-//               <a href={url} className="btn btn-danger" target="_blank">Read more!</a>
-//             </div>
-//           </div>
-//         </div>
-//       )
-//     });
-//     return (
-//       <div className="col-md-10 col-md-offset-1">
-//         <button className="btn btn-danger" onClick={this.props.onClick}>Click me!</button>
-//         <hr className="divider"/>
-//         <div className="col-md-12">
-//           <div className="row">
-//             {/* Pass in the loader via props.*/}
-//             { this.props.loader }
-//             {/* Pass in the renderFeed object. */}
-//             { renderFeed }
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-// class Loader extends Component {
-//   render() {
-//     return (
-//       <div className="col-md-12">
-//         <div className="loader"></div>
-//       </div>
-//     )
-//   }
-// }
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       randomFeed: [],
-//       feed: [],
-//       loader: <Loader/>,
-//       fav: ''
-//     }
-//     this.randomizer = this.randomizer.bind(this);
-//   }
-//   randomizer() {
-//     // We want to render a set of six random news headlines from the API data.
-//     // We can simply use Math.floor() to create a random set of healines, but
-//     // one side effect is that we get duplicates. Therefore, we use the method below to
-//     // remove duplicates.
-//     let newsFeed = this.state.feed;
-//     let tmp = newsFeed.slice(newsFeed);
-//     let randomArray = [];
-//     // We use a for loop to iterate over the length of the array six time.
-//     // Then we grab a random news headline in each iteration, removing duplicates.
-//     for (let i = 0; i < 6; i++) {
-//       let index = Math.floor(Math.random() * tmp.length);
-//       let removed = tmp.splice(index, 1);
-//       // Since we are only removing one element
-//       randomArray.push(removed[0]);
-//     }
-//     // Set state. When the button is clicked, set the state for randomFeed and the loader.
-//       this.setState({
-//         randomFeed: randomArray,
-//         loader: ''
-//       })
-//   }
-//   componentWillMount() {
-//   // Set your API URL with the API key.
-//   let url = `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${apiKey}`
-//   // We use regex to extract website name.
-//   let extract = url.match(/source=\=*(.*?)\s*&s/).pop();
-//   // We set site name to state.
-//   this.setState({site: extract})
-//   // Fetch data from API
-//   fetch(url).then((response) => {
-//     return response.json()
-//   }).then((data) => {
-//     let articles = data.articles;
-//     this.setState({feed: articles})
-//   });
-// }
-// render() {
-//   return (
-//     <div className="App row">
-//       <div className="col-md-12 hd">
-//         <h1 className="hd-title">{this.state.site}</h1>
-//         <h2 className="hd-sub">News Randomizer</h2>
-//       </div>
-//       {/* Share state with the child*/}
-//       <Feed
-//         feed={this.state.randomFeed}
-//         onClick={this.randomizer}
-//         loader={this.state.loader}
-//       />
-//     </div>
-//   );
-// }
-// }
-// export default App;
---
+++ src/components/CatBody/CatCards.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+class CatCard extends Component {
+  render() {
+    let categories = this.props.categories;
+    let etsyListings = this.props.etsyListings;
+    let catCard = categories.map((listing) => {
+      let filteredListings = etsyListings.filter((filteredListing) => {
+          return filteredListing.taxonomy_path.includes(listing.category.name);
+        });
+      let randomListing = filteredListings[Math.floor(Math.random() * filteredListings.length)];
+      return (
+        <div key={randomListing.id} className = "col-md-3">
+          <div className="card" >
+            <a href={randomListing.url}><img className="card-img-top" src={randomListing.MainImage.url_170x135} alt="random jewelry" /></a>
+            <div className="card-block">
+              <h2 className="card-title">
+                <a href={"https://www.etsy.com/c/" + listing.category.path}>{listing.category.name}</a>
+              </h2>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div className="row">
+        {catCard}
+      </div>
+    )
+  }
+}
+
+export default CatCard
