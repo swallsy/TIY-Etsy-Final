b6535e79c099c46a223f299398cd4ad201325eae
Jason Grigg <boatdreamin@gmail.com>
2017-07-27 15:45:41 -0400

    ProductCards star creation and star files

--- src/components/CatBody/ProductCards.js
+++ src/components/CatBody/ProductCards.js
@@ -6,58 +6,59 @@ export default class ProductCards extends Component {
     this.state= {
     feedback_score: '',
     feedbackstar: '',
-    starfilled: <img src='http://www.iconsdb.com/icons/preview/orange/star-xxl.png'/>,
-            starunfilled: <img src='http://www.iconsdb.com/icons/preview/gray/star-xxl.png'/>
+    starfilled: <img src={'star_full.png'}/>,
+  starunfilled: <img src={'star_empty'}/>
   }
 }
 
-// this.setState({feedback_score: response.results[0].Shop.User.feedback_info.score});
-// if (this.state.feedback_score > 89) {
-//     this.setState({
-//         feedbackstar: <div>
-//                 {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}
-//             </div>
-//     })
-// } else if (this.state.feedback_score  > 69) {
-//     this.setState({
-//         feedbackstar: <div>
-//                 {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}
-//             </div>
-//     })
-// } else if (this.state.feedback_score  > 49) {
-//     this.setState({
-//         feedbackstar: <div>
-//                 {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}
-//             </div>
-//     })
-// } else if (this.state.feedback_score  > 29) {
-//         this.setState({
-//             feedbackstar: <div>
-//                     {this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
-//                 </div>
-//         })
-//     } else if (this.state.feedback_score  > 9) {
-//         this.setState({
-//             feedbackstar: <div>
-//                     {this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
-//                 </div>
-//         })
-//     } else {
-//         this.setState({
-//             feedbackstar: <div>
-//                     {this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
-//                 </div>
-//         })
-//     }
 
   render() {
       let productCards = this.props.listingFilter.map((product) => {
+        this.setState({feedback_score: product.User.feedback_info.score});
+        if (this.state.feedback_score > 89) {
+            this.setState({
+                feedbackstar: <div>
+                        {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}
+                    </div>
+            })
+        } else if (this.state.feedback_score  > 69) {
+            this.setState({
+                feedbackstar: <div>
+                        {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}
+                    </div>
+            })
+        } else if (this.state.feedback_score  > 49) {
+            this.setState({
+                feedbackstar: <div>
+                        {this.state.starfilled}{this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}
+                    </div>
+            })
+        } else if (this.state.feedback_score  > 29) {
+                this.setState({
+                    feedbackstar: <div>
+                            {this.state.starfilled}{this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
+                        </div>
+                })
+            } else if (this.state.feedback_score  > 9) {
+                this.setState({
+                    feedbackstar: <div>
+                            {this.state.starfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
+                        </div>
+                })
+            } else {
+                this.setState({
+                    feedbackstar: <div>
+                            {this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}{this.state.starunfilled}
+                        </div>
+                })
+            }
+
         return (
           <div className="card col-md-2" key={product.listing_image_id}>
             <img src={product.MainImage.url_170x135} />
               <div>{product.title.substring(0, 36)}...</div>
               <div>{product.Shop.shop_name}</div>
-              <div>({product.User.feedback_info.score} out of 100)({product.num_favorers})</div>
+              <div>{this.state.feedbackstar}({product.num_favorers})</div>
               <div>${product.price}</div>
           </div>
         )
