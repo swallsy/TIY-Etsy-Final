76a34b3fde5933015f0ec1a56120a60ffb53f197
Robert Hutchins <rjhutchins22@gmail.com>
2017-07-27 10:56:08 -0400

    All the stuff we worked on yesterday

--- src/components/CatBar/Price.js
+++ src/components/CatBar/Price.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Price extends React.Component {
-
-
-
+  constructor(props){
+    super(props);
+  }
 
   render() {
     return (
       <form>
-      <div>
-      <label>
-        Price ($)
-      </label>
-      </div>
-    <div className="price">
-      <label>
-        <input type="radio" value="option1" checked={true} />
-        Under $25
-      </label>
-    </div>
-    <div className="price">
-      <label>
-        <input type="radio" value="option2" />
-        $25 to $50
-      </label>
-    </div>
-    <div className="price">
-      <label>
-        <input type="radio" value="option3" />
-        $50 to $100
-      </label>
-    </div>
-    <div className="price">
-      <label>
-        <input type="radio" value="option3" />
-        Over $100
-      </label>
-    </div>
-    <div className="price">
-      <label>
-        <input type="radio" value="option3" />
-        Custom
-      </label>
-    </div>
-    <input className="titleForm" type="text" value="Low"/>
-    <label> to </label>
-    <input className="titleForm" type="text" value="High"/>
-    <input className="infoSubButton" type="submit" value="&rarr;" />
-  </form>
+        <div>
+          <label>
+            Price ($)
+          </label>
+        </div>
+        <div className="price">
+          <label>
+            <input onChange={this.props.handlePriceSelection} type="radio" name="25 and Under" value="option" />
+            Under $25
+          </label>
+        </div>
+        <div className="price">
+          <label>
+            <input type="radio" value="option2"/>
+            $25 to $50
+          </label>
+        </div>
+        <div className="price">
+          <label>
+            <input type="radio" value="option3"/>
+            $50 to $100
+          </label>
+        </div>
+        <div className="price">
+          <label>
+            <input type="radio" value="option3"/>
+            Over $100
+          </label>
+        </div>
+        <div className="price">
+          <label>
+            <input type="radio" value="option3"/>
+            Custom
+          </label>
+        </div>
+        <input className="titleForm" type="text" value="Low"/>
+        <label>
+          to
+        </label>
+        <input className="titleForm" type="text" value="High"/>
+        <input className="infoSubButton" type="submit" value="&rarr;"/>
+      </form>
     )
   }
 }
--- src/components/CatBody/ProductCards.js
+++ src/components/CatBody/ProductCards.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
 
 export default class ProductCards extends Component {
-  constructor() {
-    super();
-    this.state = {
-      products : []
-    };
+  constructor(props) {
+    super(props);
   }
-
-  componentDidMount() {
-    fetch('https://api.etsy.com/v2/listings/trending?api_key=3yhxu7gn2ot24so9hzuqbxc9&limit=10&offset=0&page=1&categories=jewelry,craft_supplies_and_tools,home_and_living,art_and_collectibles,clothing,accessories,paper_and_party_supplies,weddings,bags_and_purses&includes=MainImage,Shop').then(resp => resp.json())
-    .then(resp => {
-      let productCards = resp.results.map((product) => {
+  render() {
+      let productCards = this.props.listingFilter.map((product) => {
         return (
-          <div className="card col-md-3" key={product.MainImage.listing_image_id}>
+          <div className="card col-md-2" key={product.listing_image_id}>
             <img src={product.MainImage.url_170x135} />
-            <div>{product.title}</div>
-            <div>{product.Shop.shop_name}</div>
-            <div>({product.Shop.num_favorers})</div>
-            <div>${product.price}</div>
+              <div>{product.title}</div>
+              <div>{product.price}</div>
           </div>
         )
       })
-      this.setState({products: productCards});
-      console.log(this.state.products);
-    })
-  }
-
-
-  render() {
     return (
       <div className="row">
-        {this.state.products}
+        {productCards}
       </div>
     )
   }
