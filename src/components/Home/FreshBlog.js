dbb71e6f0a17200ab34ea9d377610a5ecf3661f6
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-07-30 14:08:19 -0400

    add some stylings to orange bars and linked the whole orange bar.

--- src/components/Home/FreshBlog.js
+++ src/components/Home/FreshBlog.js
@@ -11,7 +11,7 @@ class FreshBlog extends Component {
           <div className="card blogcard">
             <div className="card-block">
               <a href="https://blog.etsy.com/en/featured-shop-emma-claire/?ref=hp" alt="featured shop by emma claire. Blog Post.">
-                <img className="card-img-top blogimg" src={require("./images/etsypic1.jpg")} alt="Card image cap"/>
+                <img className="card-img-top blogimg" src={require("./images/etsypic1.jpg")} alt="glass container holding a plant and some sand. All hanging from a brown wooden fence."/>
                 <div className="blogtext"><h5 className="card-text blogcategories">FEATURED SHOPS</h5>
                 <h4 className="card-title texttitle">Featured Shop: Emma Claire</h4>
                 <p className="card-text blogparagraph">
@@ -27,7 +27,7 @@ class FreshBlog extends Component {
           <div className="card blogcard">
             <div className="card-block">
               <a href="https://blog.etsy.com/en/are-you-pro-pineapple-or-team-watermelon/?ref=hp" alt="Shopping guides, are you pro pineapple or watermelon">
-                <img className="card-img-top blogimg" src={require("./images/etsypic2.jpg")} alt="Card image cap"/>
+                <img className="card-img-top blogimg" src={require("./images/etsypic2.jpg")} alt="pineapple and watermelon clutch purses."/>
                 <div className="blogtext"><h5 className="card-text blogcategories">SHOPPING GUIDES</h5>
                 <h4 className="card-title texttitle">Are You Pro-Pineapple or Team Watermelon?</h4>
                 <p className="card-text blogparagraph">
@@ -43,7 +43,7 @@ class FreshBlog extends Component {
           <div className="card blogcard">
             <div className="card-block">
               <a href="https://blog.etsy.com/en/the-ultimate-diy-birthday-party-playbook/?ref=hp" alt="Inspiration, the ultimate diy birthday party playbook.">
-                <img className="card-img-top blogimg" src={require("./images/etsypic3.jpg")} alt="Card image cap"/>
+                <img className="card-img-top blogimg" src={require("./images/etsypic3.jpg")} alt="A wide display of ice cream cones and sprinkles and other ice cream party items."/>
                 <div className="blogtext"><h5 className="card-text blogcategories">INSPIRATION</h5>
                 <h4 className="card-title texttitle">The Ultimate DIY Birthday Party Playbook</h4>
                 <p className="card-text blogparagraph">
@@ -54,7 +54,7 @@ class FreshBlog extends Component {
           </div>
         </div>
       </div>
-      <div className="bottomlinktoblog"><a className="bloglink" href="https://blog.etsy.com/en?ref=hpsm">Read the blog <i className="fa fa-chevron-right"></i></a></div>
+      <div className="bottomlinktoblog"><a className="bloglink" href="https://blog.etsy.com/en?ref=hpsm" alt="Read the blog">Read the blog <i className="fa fa-chevron-right"></i></a></div>
     </div>
     );
   }
